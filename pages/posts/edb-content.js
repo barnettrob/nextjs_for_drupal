8ffc569ab25130ad3b70295acc3b58a99df18985
Robert Barnett <robert.barnett@laptop210.ma.us>
2020-05-25 16:37:18 -0400

    add bootstrap styling to look like uxteam mockup

--- pages/posts/edb-content.js
+++ pages/posts/edb-content.js
@@ -1,31 +1,56 @@
 import Link from 'next/link'
 // npm install --save isomorphic-unfetch
 import fetch from 'isomorphic-unfetch'
+import Head from 'next/head'
+import PlaceholderSvg from '../placeholder-svg'
 
 function EdbContent(props) {
     console.warn("data", props.data.data.nodeQuery.entities)
     return (
         <>
-            <h1>Fetch EDB Tutorials</h1>
+            <Head>
+                <title>EDB React Bootstrap</title>
+                <link rel="stylesheet" href="https://edb-ux.herokuapp.com/edb-ivory-css/bootstrap-tweaked-20200421.min.css"></link>
+            </Head>
+            <div className="container text-center my-0">
+              <span className="img-fluid opacity-1">
+                  <PlaceholderSvg />
+              </span>
+              <h1 className="balance-text">Postgres Tutorials</h1>
+              <p className="text-muted balance-text">
+                  This text supports the headline above. Donec id elit non mi porta gravida at eget metus.<br data-owner="balance-text"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed.
+              </p>
+            </div>
+            <div className="container my-5">
+              <div className="row">
+                <div className="col-md-9 col-sm-12 mx-auto">
+                <h2 className="text-sm-left text-center mb-4">Recent Tutorials</h2>  
             {
                 props.data.data.nodeQuery.entities.map(data => (
-                    <div key={data.entityId}>
-                        <Link as={''} href={`#`}>
-                            <a>{data.entityLabel}</a>
-                        </Link>
-                        <div>{data.fieldAuthoredByRef.entity.entityLabel}</div>
-                        <div>{data.body.summary}</div>
+                    <div key={data.entityId} className="d-flex align-items-center border-top position-relative py-4 flex-column flex-sm-row text-center text-sm-left">
+                        <div className="py-2">
+                        <h3 className="m-0"> 
+                            <Link as={''} href={`#`}>
+                                <a className="text-dark stretched-link">{data.entityLabel}</a>
+                            </Link>
+                        </h3>
+                        <p className="mb-0">{data.fieldAuthoredByRef.entity.entityLabel}</p>
+                        <p className="mb-0 text-muted small">{data.body.summary}</p>
                         <br />
+                        </div>
                     </div>
                 ))
             }
+                </div>
+            </div>
+          </div>
         </>
     )
 }
 
 
 EdbContent.getInitialProps = async function () {
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTAyNDIxNTQsImV4cCI6MTU5MDI0NTc1NCwiZHJ1cGFsIjp7InVpZCI6IjQ4NDI4MSJ9fQ.v8waA2Ovj8LlVGeiISxkXKNXqiKSIBV2vmB1xk0X8RyLkymjHFPnf0tUITJO6GVS4xHDlEwN-svLybIOZPzWUuI39mwUpF9ecsmhK9gTaq-pbPmsNw67fHkhhoJQtCCbgoFj8Jl1DcLXNtaN5G95c4fgA9OsBKyKvipkpNKV0_V0EPGNWRHwNOKCISoZjQvoeadUsa1K0EOb78dR7qj-SfOgS1O6Th48tNBEOTcp_T3ga3yXlTpcn59vwt19RTtPrPHbW6NfJ8tJQTv-nTcH5vstHMNqzMDKu9SmAvpb6ORZnSYXd1uaBNC0bGAK7xsBs76w98iHq-DMHphuskwqmg'
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTA0MTM5NzQsImRydXBhbCI6eyJ1aWQiOiI0ODQyODEifX0.p2IA3KiU4nRqvQwMdBoMA_Rb9KKosvLy1MFzrZfdp-moYgcH6JNhJgml8gsQhlXMt1EKYIRjxxbVjUoaMA3P08WszSeGJ5zUV_QXGGonVs0VZiH8zg9RiNp1-tvK9B1NibDSA2OiLia_Xb7BAgT9It6Ioqgtd3IcceczLpYc7mprXd2R133j1QibmYmQEYIgs78SyZvutKULDxbqL5X2ixCixGSw29OMX-Nhs6fK2Q9X43mrREJQW6LmTtnIPaMpZTNbHi5dPrWe8opDN_CmtpyA4mLnu9n9c3xPLCORITu_zICVf-Tk9Iam-ppFzDBYK-Y9w5OCu-yFTv7_5MIjrw'
     //const res = await fetch('https://jsonplaceholder.typicode.com/todos')
     //const url 'http://uxteam:neo2020@redesign.docksal/jsonapi/node/tutorials_?page[limit]=10&filter[status][value]=1'
     const url = 'http://headless.docksal/graphql?queryId=a1e0b3f4019e514a581d765431ac078831357500:1'
