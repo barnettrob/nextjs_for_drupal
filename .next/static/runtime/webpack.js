0bb2cdea6fdd6ec007d425616731bb550ce731fe
Sunil Arora <sunil.arora@enterprisedb.com>
2020-05-26 10:38:29 +0530

    Adding next js files

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6a2dcd94034c2fef1542";
+/******/ 	var hotCurrentHash = "682efa0b996cf347e776";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
