b699653abf663eb7e1be50c24982e7aae9cfe41c
Robert Barnett <robert.barnett@laptop210.ma.us>
2020-05-31 13:44:57 -0400

    add signika font and individual node page...still building this out.

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="prefetch"
+                        href="/fonts/signika-vf.woff2"
+                        type="font/woff2"
+                        as="font"
+                        crossOrigin=""
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
---
+++ pages/back-svg.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function backSvg() {
+    return (
+        <svg title="arrow-left" className="fill-black mt-n1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="12" height="12" viewBox="0 0 24 24"><path d="M 10 4.9296875 L 2.9296875 12 L 10 19.070312 L 11.5 17.570312 L 6.9296875 13 L 21 13 L 21 11 L 6.9296875 11 L 11.5 6.4296875 L 10 4.9296875 z"></path></svg>
+    )
+}
+
+export default backSvg
